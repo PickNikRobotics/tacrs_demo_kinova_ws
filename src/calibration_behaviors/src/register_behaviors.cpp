1702e6bdc2dd94312c73818eacc62f0ea8b20e52
Richard Pratt <43280770+rlpratt12@users.noreply.github.com>
2025-03-06 11:57:02 -0700

    Add fuse and calibration objective to hw config
    * Add calibration tool to urdf and add behavior
    
    * Add initial calibration objective files
    
    * Add fuse launch and config to hw config. Needs to be tested on HW.
    
    * Add calibration_behaviors and testing objectives
    
    * Fix calibration file save path
    
    * Add average and pose to rpy file behaviors to objective.
    
    * Single tag no offset works pretty well, some delay.
    
    * All tag tf's corrected and working. Weighting meas way more than model.
    
    * Corrected average orientation error (z and w were switched). Also updated the save calibration yaml node name to match the import to xacro.
    
    * Updated ros2 control param to fix the 0-angle wrap problem with waypoints failing. Note that this is needed because we use use_external_cable=false despite having cables, because =true causes undesirable/unexpected trajectories.
    
    * Updated cal objective for more waypoints, and hw-specific grapple objective to add on-start delay. Also added hardware-specific joint_limits.
    
    ---------
    
    Co-authored-by: David Yackzan <dwyackzan@gmail.com>

---
+++ src/calibration_behaviors/src/register_behaviors.cpp
@@ -0,0 +1,27 @@
+#include "behaviortree_cpp/bt_factory.h"
+#include "moveit_studio_behavior_interface/behavior_context.hpp"
+#include "moveit_studio_behavior_interface/shared_resources_node_loader.hpp"
+
+#include "calibration_behaviors/average_pose_stamped_vector.hpp"
+#include "calibration_behaviors/calibrate_camera_pose.hpp"
+#include "calibration_behaviors/save_calibration_pose_yaml.hpp"
+
+#include "pluginlib/class_list_macros.hpp"
+
+namespace calibration_behaviors
+{
+class CalibrationBehaviorsLoader : public moveit_studio::behaviors::SharedResourcesNodeLoaderBase
+{
+public:
+  void registerBehaviors(BT::BehaviorTreeFactory &factory,
+                         const std::shared_ptr<moveit_studio::behaviors::BehaviorContext> &shared_resources) override
+  {
+    moveit_studio::behaviors::registerBehavior<AveragePoseStampedVector>(factory, "AveragePoseStampedVector", shared_resources);
+    moveit_studio::behaviors::registerBehavior<CalibrateCameraPose>(factory, "CalibrateCameraPose", shared_resources);
+    moveit_studio::behaviors::registerBehavior<SaveCalibrationPoseYaml>(factory, "SaveCalibrationPoseYaml", shared_resources);
+  }
+};
+}  // namespace calibrate_camera_pose
+
+PLUGINLIB_EXPORT_CLASS(calibration_behaviors::CalibrationBehaviorsLoader,
+                       moveit_studio::behaviors::SharedResourcesNodeLoaderBase);
